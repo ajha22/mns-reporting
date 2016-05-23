9ee7ed97b196d5ec517fb3791c47ecd5b8a46b62
ABC <XYZ>
2016-05-23 18:00:26 +0530

    risk page created, populated with mockdata

--- src/js/riskIssuesController.js
+++ src/js/riskIssuesController.js
@@ -1,12 +1,10 @@
 var app = angular.module("weeklyReports", []);
 app.controller("riskIssuesCtrl", function($scope,$http) {
-      console.log("hheyyyy1");
     $http({
         method : "GET",
-        url : "../mockData"
+        url : "../mockData/riskIssues.json"
     }).then(function mySucces(response) {
-        $scope.myWelcome = response.data;
-        console.log("hheyyyy2",$scope.myWelcome);
+        $scope.riskReport = response.data;
       });
 
 });
